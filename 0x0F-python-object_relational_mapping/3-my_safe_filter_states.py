a5ca82c1d93688b9a65d2f25cadfd63a69adbde2
papaseyone <charleskumahntim@gmail.com>
2024-03-19 18:12:40 -0700

    -python-object_relational_mapping

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This script takes in an arg and
+shows all values in the states
+where the  `name` matches the arg
+from the database `hbtn_0e_0_usa`.
+The script is safe from
+MySQL injections!
+"""
+
+import MySQLdb as db
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Get access to the database and that of the states
+    from the database.
+    """
+    db_connect = db.connect(host="localhost", port=3306,
+                            user=argv[1], passwd=argv[2], db=argv[3])
+
+    db_cursor = db_connect.cursor()
+    db_cursor.execute(
+        "SELECT * FROM states WHERE name LIKE \
+                    BINARY %(name)s ORDER BY states.id ASC", {'name': argv[4]})
+
+    rows_selected = db_cursor.fetchall()
+
+    for row in rows_selected:
+        print(row)
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,12 @@
+ Python - Object-relational mapping
+
+At the end of this project, you are expected to be able to explain and unertale task in the ff:
+
+
+Why Python programming is awesome
+How to connect to a MySQL database from a Python script
+How to SELECT rows in a MySQL table from a Python script
+How to INSERT rows in a MySQL table from a Python script
+What ORM means
+How to map a Python Class to a MySQL table
+How to create a Python Virtual Environment
